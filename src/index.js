02e0571350e98770c5f183fc8d97c4e1cbfa2cb3
Zulma Rocio <zrmartinezg@gmail.com>
2021-10-19 23:31:53 -0500

    Remove unnecessary files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
 ReactDOM.render(
