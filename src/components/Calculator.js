6b517e78e06f87b49e00a3ab0dfdb045ea189451
Zulma Rocio <zrmartinezg@gmail.com>
2021-10-19 20:02:14 -0500

    Add style with bootstrap

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,34 +3,35 @@ import React from 'react';
 export default class Calculator extends React.PureComponent {
   render() {
     return (
-      <div className="calculator">
-        <div className="result">
-          0
+      <div className="calculator row">
+        <div className="result float-right">
+          <p className="py-1">0</p>
         </div>
-        <div className="calculator-operators">
-          <button type="button">/</button>
-          <button type="button">x</button>
-          <button type="button">-</button>
-          <button type="button">+</button>
-          <button type="button">=</button>
-        </div>
+        <div className="calculator-buttons row">
+          <div className="calculator-digits container col row">
+            <button type="button" className=" col-4">AC</button>
+            <button type="button" className=" col-4">+/-</button>
+            <button type="button" className=" col-4">%</button>
+            <button type="button" className=" col-4">7</button>
+            <button type="button" className=" col-4">8</button>
+            <button type="button" className=" col-4">9</button>
+            <button type="button" className=" col-4">4</button>
+            <button type="button" className=" col-4">5</button>
+            <button type="button" className=" col-4">6</button>
+            <button type="button" className=" col-4">1</button>
+            <button type="button" className=" col-4">2</button>
+            <button type="button" className=" col-4">3</button>
+            <button type="button" className=" col-10">0</button>
+            <button type="button" className=" col-2">.</button>
 
-        <div className="calculator-digits">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button">+</button>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button">0</button>
-          <button type="button">.</button>
+          </div>
+          <div className="calculator-operators container col-2 row">
+            <button type="button">/</button>
+            <button type="button">x</button>
+            <button type="button">-</button>
+            <button type="button">+</button>
+            <button type="button">=</button>
+          </div>
 
         </div>
       </div>
