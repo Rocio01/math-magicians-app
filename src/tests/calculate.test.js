d4c0efc73808635cefc134678a0c149f33b4197f
Abel Lavieri <abel.lavieri@gmail.com>
2021-10-27 18:18:21 -0400

    Add tests for sign change and mod operators

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -34,6 +34,18 @@ describe('it tests the addition function properly', () => {
     calcObj = calculate(calcObj, '=');
     expect(calcObj.total).toBe('3.6');
   });
+
+  test('it retrieves the correct decimal numbers', () => {
+    calcObj = calculate(calcObj, '2');
+    calcObj = calculate(calcObj, '.');
+    calcObj = calculate(calcObj, '5');
+    calcObj = calculate(calcObj, '+');
+    calcObj = calculate(calcObj, '2');
+    calcObj = calculate(calcObj, '.');
+    calcObj = calculate(calcObj, '1');
+    calcObj = calculate(calcObj, '=');
+    expect(calcObj.total).not.toBe('3.6');
+  });
 });
 
 describe('it tests the substraction to function properly', () => {
@@ -63,6 +75,18 @@ describe('it tests the substraction to function properly', () => {
     calcObj = calculate(calcObj, '1');
     calcObj = calculate(calcObj, '=');
     expect(calcObj.total).toBe('2.4');
+  });
+
+  test('it retrieves the correct decimal numbers', () => {
+    calcObj = calculate(calcObj, '2');
+    calcObj = calculate(calcObj, '.');
+    calcObj = calculate(calcObj, '5');
+    calcObj = calculate(calcObj, '-');
+    calcObj = calculate(calcObj, '2');
+    calcObj = calculate(calcObj, '.');
+    calcObj = calculate(calcObj, '1');
+    calcObj = calculate(calcObj, '=');
+    expect(calcObj.total).not.toBe('3.6');
   });
 });
 
@@ -155,5 +179,61 @@ describe('it tests the division function properly', () => {
     calcObj = calculate(calcObj, '0');
     calcObj = calculate(calcObj, '=');
     expect(calcObj.total).toBe("Can't divide by 0.");
+  });
+});
+
+describe('it tests the sign change', ()=>{
+  test('it changes the + sign of a single number', () => {
+    calcObj = calculate(calcObj, '10');
+    calcObj = calculate(calcObj, '+/-');
+    expect(calcObj.next).toBe('-10');
+  });
+
+  test('it changes the + sign of a nomber after an operation', () => {
+    calcObj = calculate(calcObj, '10');
+    calcObj = calculate(calcObj, '+');
+    calcObj = calculate(calcObj, '2');
+    calcObj = calculate(calcObj, '=');
+    calcObj = calculate(calcObj, '+/-');
+    expect(calcObj.total).toBe('-12');
+  });
+
+  test('it changes the - sign of a single number', () => {
+    calcObj = calculate(calcObj, '-10');
+    calcObj = calculate(calcObj, '+/-');
+    expect(calcObj.next).toBe('10');
+  });
+
+  test('it changes the - sign of a nomber after an operation', () => {
+    calcObj = calculate(calcObj, '10');
+    calcObj = calculate(calcObj, '-');
+    calcObj = calculate(calcObj, '20');
+    calcObj = calculate(calcObj, '=');
+    calcObj = calculate(calcObj, '+/-');
+    expect(calcObj.total).toBe('10');
+  });
+
+  test('it retrieves the correct altered number', () => {
+    calcObj = calculate(calcObj, '20');
+    calcObj = calculate(calcObj, '+/-');
+    expect(calcObj.next).not.toBe('-15');
+  });  
+});
+
+describe('it tests the mod operation', ()=>{
+  test('it gives the proper mod', ()=>{
+    calcObj = calculate(calcObj, '10');
+    calcObj = calculate(calcObj, '%');
+    calcObj = calculate(calcObj, '2');
+    calcObj = calculate(calcObj, '=');
+    expect(calcObj.total).toBe('0');
+  });
+
+  test('it gives the proper mod', ()=>{
+    calcObj = calculate(calcObj, '10');
+    calcObj = calculate(calcObj, '%');
+    calcObj = calculate(calcObj, '3');
+    calcObj = calculate(calcObj, '=');
+    expect(calcObj.total).not.toBe('0');
   });
 });
